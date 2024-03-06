b365602fcfd16a7d77ef1c6e25d31f8d03aa246b
novitariyanti123 <162391091+novitariyanti123@users.noreply.github.com>
2024-03-06 08:11:16 +0700

    Add files via upload

---
+++ registrasi.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman Registrasi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <h2>PERPUSTAKAAN DIGITAL</h2>
+    <div class="container">
+      <div class="content">
+        <div class="card" style="margin-top:1rem;">
+          <div class="row">
+            <div class="col m-3">
+              <h2>Silahkan Masukkan data anda Untuk Melanjutkkan!</h2>
+              <form action="" method="post">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">NIS</label>
+      <input type="text" class="form-control" required id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Nama Lengkap</label>
+      <input type="text" class="form-control" required id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div><div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email</label>
+      <input type="text" class="form-control" required id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Alamat</label>
+      <input type="text" class="form-control" required id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Username</label>
+      <input type="text" class="form-control" required id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" required id="exampleInputPassword1">
+    </div>
+    <button type="submit" class="btn btn-primary">Login</button>
+  </form>
+  <p class="mt-3">Saya belum memiliki akun, Buat <a href="registrasi.php" class="btn btn-warning text-white">Disini</a></p>
+            </div>
+            <div class="col">
+              <img src="asset/img/login.png" width="500"alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+          </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
